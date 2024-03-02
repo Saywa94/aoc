f5c20aeac21633dca9c5671c52de3811fc69a8dd
Saywa94 <saywachavez@gmail.com>
2024-03-02 18:12:20 -0400

    Started day 5 2022

---
+++ source/2022/day5.ts
@@ -0,0 +1,13 @@
+const file = Bun.file('./input/day5.txt');
+const input = await file.text();
+
+const [startingConfig, instructions] = input.split('\n\n')
+
+console.log(startingConfig.split('\n'))
+
+
+
+
+
+console.log(instructions)
+
