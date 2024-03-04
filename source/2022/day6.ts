d06418197755ce9bc3cdfcb684a3a9fd469b7d0a
Saywa94 <saywachavez@gmail.com>
2024-03-04 10:23:11 -0400

    Day 6 2022

---
+++ source/2022/day6.ts
@@ -0,0 +1,21 @@
+const file = Bun.file('./input/day6.txt');
+const input = await file.text();
+
+const dataStream = input.trim().split('')
+
+// Part 1
+for (let i = 0; i < dataStream.length; i++) {
+    if (new Set(dataStream.slice(i, i + 4)).size === 4) {
+        console.log(i + 4)
+        break
+    }
+}
+
+// Part 2
+
+for (let i = 0; i < dataStream.length; i++) {
+    if (new Set(dataStream.slice(i, i + 14)).size === 14) {
+        console.log(i + 14)
+        break
+    }
+}
